f577f6f959f10bdfc8592fcb1bae77ef2b977375
phild8 <pjsdavs@gmail.com>
2017-10-24 15:35:44 -0700

    Added Splash layout

--- app/src/main/java/edu/orangecoastcollege/cs273/alohamusic/SplashActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/alohamusic/SplashActivity.java
@@ -1,7 +1,11 @@
 package edu.orangecoastcollege.cs273.alohamusic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Create a TimerTask to defer the loading of MusicActivity by 3 seconds
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // Finish the current splash activity, then:
+                finish();
+                // Launch the MusicActivity
+                Intent musicIntent = new Intent(SplashActivity.this, MusicActivity.class);
+                startActivity(musicIntent);
+            }
+        };
+
+        // Run the timer task after 3 seconds (3000ms)
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
     }
 }
